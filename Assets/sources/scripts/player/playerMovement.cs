5f392c3d885385fab5dc02994c1a8aace54eed28
KnoxZZ <31701643+KnoxZZ@users.noreply.github.com>
2017-10-25 12:20:38 +0300

    alpha 0.0.0.1

--- Assets/sources/scripts/player/playerMovement.cs
+++ Assets/sources/scripts/player/playerMovement.cs
@@ -1,11 +1,12 @@
 ﻿using UnityEngine;
 
-public class playerMovement : MonoBehaviour
-{
-    void Update()
-    {
-        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
-        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+public class playerMovement : MonoBehaviour {
+    public int vSpeed;
+    public int hSpeed;
+
+    void Update() {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * hSpeed;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * vSpeed;
 
         transform.Rotate(0, x, 0);
         transform.Translate(0, 0, z);
