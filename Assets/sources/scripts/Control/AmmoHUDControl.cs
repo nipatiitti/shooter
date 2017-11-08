2ab9208b685f61b9ec837161e60e367dcac4a3a1
KnoxZZ <31701643+KnoxZZ@users.noreply.github.com>
2017-11-08 15:39:18 +0200

    .

---
+++ Assets/sources/scripts/Control/AmmoHUDControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+[AddComponentMenu("Control/Ammo")]
+public class AmmoHUDControl : MonoBehaviour {
+
+    public GameObject ammoText;
+    Text Teksti;
+    GameObject GunControl;
+
+    void Awake()
+    {
+        GunControl = GameObject.Find("gunController");
+        Teksti = ammoText.GetComponent<Text>();
+    }
+
+ 
+
+	void Update () {
+        if (GunControl.transform.childCount > 0 && GunControl.transform.GetChild(0).GetComponent<enablePicking>().canShoot)
+        {
+            int[] bullets = GunControl.transform.GetChild(0).GetComponent<basicShooting>().getBullets();
+            Teksti.text = bullets[1].ToString() + "/" + bullets[0].ToString();
+        }
+        else
+        {
+            Teksti.text = "";
+        }
+	}
+}
